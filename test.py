0edfcda6632a244f029b54b964b4b5fb7b15ab0a
xh-g <116259501+xh-g@users.noreply.github.com>
2023-05-25 21:44:05 +0800

    Update test.py
    add new content

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
  
-fruits = ['banana', 'apple',  'mango']
+fruits = ['banana', 'apple',  'mango','watermelon']
 for index in range(len(fruits)):
    print ('当前水果 : %s' % fruits[index])
  
